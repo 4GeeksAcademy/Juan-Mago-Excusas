5a5a0b1ea098109b5da2a864d3af1d49fa374b17
Jfmm1983 <123413274+Jfmm1983@users.noreply.github.com>
2024-12-30 01:21:46 +0000

    feat/index.html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomRangeNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let quien = who[randomRangeNumber(0, 3)];
+let accion = action[randomRangeNumber(0, 3)];
+let que = what[randomRangeNumber(0, 2)];
+let cuando = when[randomRangeNumber(0, 4)];
+
+let excusa = quien + " " + (accion + " ") + (que + " ") + (cuando + " ");
+
+function myFunction() {
+  document.getElementById("excuse").innerHTML = excusa;
+  document.getElementById("titulo").innerHTML = "La excusa generada es:";
+}
+
+const reload = document.getElementById("reload");
+reload.addEventListener("click", _ => {
+  location.reload();
+});
